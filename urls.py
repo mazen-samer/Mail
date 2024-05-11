d8e2ba5e8a68c4e74b0db36f9c375604623e28a4
Mazen Samer Abdallah <93529760+mazen-samer@users.noreply.github.com>
2024-05-11 22:33:05 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 
 from . import views
-
+ # API Routes
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-
-    # API Routes
     path("emails", views.compose, name="compose"),
     path("emails/<int:email_id>", views.email, name="email"),
     path("emails/<str:mailbox>", views.mailbox, name="mailbox"),
